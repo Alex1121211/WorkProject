c43566318c8e00bad691c899de95d8e9c072d06b
Alex1121211 <AlexGrech98@gmail.com>
2023-04-09 23:27:31 +0300

    Добавил страницу

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 
 
 def index(request):
-    return render(request,'women/index.html')
+    return render(request, 'women/index.html')
 
 def categories(request, catid):
     if(request.GET):
