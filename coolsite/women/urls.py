53c5d0dfe42078cb3976752a03f168db2fa87aff
Alex1121211 <AlexGrech98@gmail.com>
2023-04-11 23:05:48 +0300

    Маршруты+ посты

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
     path('addpage/', addpage, name='add_page'),
-    path('contact/', contact, name='cantact'),
+    path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
 ]
