cc49effb3d52a49b925eb0b86ee41a0b889a45ec
Alex1121211 <AlexGrech98@gmail.com>
2023-04-18 22:45:29 +0300

    добавил категории обаботал дату

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -40,4 +40,16 @@ def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 def show_category(request,cat_id):
-    return HttpResponse(f"Отображение категории с id = {cat_id}")
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по статьям',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
