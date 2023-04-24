665ad8577c02ac3fbceab5ef27a3cd3d25e4c491
Alex1121211 <AlexGrech98@gmail.com>
2023-04-24 21:51:40 +0300

    Пользовательские теги

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -11,10 +11,9 @@ menu =[{'title': "Добавить статью", 'url_name': 'add_page'},
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
+
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Главная страница',
         'cat_selected': 0,
@@ -41,13 +40,11 @@ def show_post(request, post_id):
 
 def show_category(request,cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404()
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Отображение по статьям',
         'cat_selected': cat_id,
